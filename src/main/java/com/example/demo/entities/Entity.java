d05a2b6a3c07dbeb04ce1dac22236892a7b92cc7
bdenisaloredana <dydylore@gmail.com>
2023-03-05 23:21:34 +0200

    specifications

--- src/main/java/com/example/demo/entities/Entity.java
+++ src/main/java/com/example/demo/entities/Entity.java
@@ -7,13 +7,28 @@ public class Entity<ID> implements Serializable {
 
     private static final long serialVersionUID = 7331115341259248461L;
     private ID id;
+
+    /***
+     * Getter for the id of the entity.
+     * @return the id of the entity
+     */
     public ID getId() {
         return id;
     }
+
+    /***
+     * Setter for the id of the entity.
+     * @param id the id of the entity
+     */
     public void setId(ID id) {
         this.id = id;
     }
 
+    /***
+     * Override for the equals function.
+     * @param o the object with which we compare
+     * @return true if the two objects are the same, false otherwise
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -22,6 +37,10 @@ public class Entity<ID> implements Serializable {
         return Objects.equals(id, entity.id);
     }
 
+    /***
+     * Override for the hasCode function.
+     * @return the hash value of the object
+     */
     @Override
     public int hashCode() {
         return Objects.hash(id);
